9e315a0d8df7da7e058dfce1ea5ca26b80bff3b4
AngeloBurt <2423986459@qq.com>
2023-04-27 08:52:49 +0800

    p5 5- 进行到11:37

--- server/apps/admin/src/admin.module.ts
+++ server/apps/admin/src/admin.module.ts
@@ -3,9 +3,10 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { DbModule } from '@libs/db';
 import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [DbModule, UsersModule],
+  imports: [DbModule, UsersModule, CoursesModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
